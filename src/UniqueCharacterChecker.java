59b6af84cccc89267495a4e5edcdae0d8cc63d6e
lilPunny <166659824+GenaroP3rez@users.noreply.github.com>
2026-06-12 21:55:13 -0700

    turrible syntax fixed file 3 and logic array fixed as well

--- src/UniqueCharacterChecker.java
+++ src/UniqueCharacterChecker.java
@@ -14,23 +14,18 @@ public class UniqueCharacterChecker {
      * @return true if all characters in the word are unique; false otherwise
      */
     public static boolean hasUniqueCharacters(String word) {
-        // TODO: implement this!
-        // Requirement: This must run in O(n) time, where n is the number of characters in the word
-        // Hint: Stuck? Consider looking up "charAt" and seeing how it can help you
-        //return false;
+        Set<Character> characters = new HashSet<>();
 
-        Set<Character> characters = mnew HashSet<>(); i++){
-           for (int i = 0; i < word; i++){
-
+        for (int i = 0; i < word.length(); i++) {
             char currentCharacter = word.charAt(i);
-           }
 
-           if (characters.contains(currentCharacter)){
-            return false;
-           }
+            if (characters.contains(currentCharacter)) {
+                return false;
+            }
 
-           characters.add(currentCharacter); 
+            characters.add(currentCharacter);
         }
+
         return true;
     }
 
@@ -39,8 +34,11 @@ public class UniqueCharacterChecker {
         String word2 = "world";
         String word3 = "abcdefg";
 
-        System.out.println(word1 + " has unique characters: " + hasUniqueCharacters(word1));
-        System.out.println(word2 + " has unique characters: " + hasUniqueCharacters(word2));
-        System.out.println(word3 + " has unique characters: " + hasUniqueCharacters(word3));
+        System.out.println(word1 + " has unique characters: "
+                + hasUniqueCharacters(word1));
+        System.out.println(word2 + " has unique characters: "
+                + hasUniqueCharacters(word2));
+        System.out.println(word3 + " has unique characters: "
+                + hasUniqueCharacters(word3));
     }
 }
